b4019f4f9c7edf9875c4ec61637dc93e1ed7a6c7
SEGARS <vim_branet@mail.ru>
2023-03-09 11:41:40 +0500

    циклы

---
+++ Project22/Project22.cpp
@@ -0,0 +1,19 @@
+/*Что такое вложенный цикл. Вложенная конструкция. C++ для начинающих. Урок #24.*/
+
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	for (int i = 1; i < 5; i++)
+	{
+		cout << "Сработал 1й цикл for интеграция № " << i << endl;
+
+		for (int j = 1; j < 5; j++)
+		{
+			cout << "\tсработал 2й цикл for интеграции № " << j << endl;
+		}
+	}
+}
