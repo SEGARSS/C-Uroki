e0a0f1651e8e81107d12799c362c672571281fd5
SEGARS <vim_branet@mail.ru>
2023-03-22 10:52:41 +0500

    дорешать

---
+++ Project27/Project27.cpp
@@ -0,0 +1,21 @@
+//sizeof что это. sizeof c++ массив. Узнать количество элементов массива. sizeof array. Урок #28.
+
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+	int arr[]{5,69,874,22,9852,63};
+	cout << sizeof(arr)/sizeof(int) << endl;
+	/*for (int i = 0; i < sizeof(arr); i++)
+	{
+		cout << arr[i] << endl;
+	}*/
+
+	//Доучи.
+
+
+}
