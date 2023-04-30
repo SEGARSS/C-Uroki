170dc15b15261c602de1008722358619ce0673ed
SEGARS <vim_branet@mail.ru>
2023-04-30 13:09:16 +0500

    57-58 урок

--- Project60/Project60.cpp
+++ Project60/Project60.cpp
@@ -1,10 +1,30 @@
-//Константные аргументы функции. Константный указатель. Константные параметры. Для начинающих Урок #57
+//Константные аргументы функции. Константный указатель. Константные параметры. Для начинающих Урок #57 - Урок #58
 #include<iostream>
 #include<string>
+#include<ctime>
 using namespace std;
+
+void FillArray(int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+
+void ShowArray(const int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
 
 void main()
 {
 	setlocale(LC_ALL, "Ru");
 
+	//Доделать.
+	
 }
