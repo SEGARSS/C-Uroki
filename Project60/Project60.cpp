a608d752107a3d536ce2496328d7a4f1c701b2d6
SEGARS <vim_branet@mail.ru>
2023-04-30 12:46:16 +0500

    60 урок

---
+++ Project60/Project60.cpp
@@ -0,0 +1,10 @@
+//Константные аргументы функции. Константный указатель. Константные параметры. Для начинающих Урок #57
+#include<iostream>
+#include<string>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Ru");
+
+}
