a45c005579e0f5dc4a2031f28c4ac677c6a368df
SEGARSS <vim_branet@mail.ru>
2023-05-11 10:59:42 +0500

    указатели и типы

---
+++ Project65/Project65.cpp
@@ -0,0 +1,31 @@
+//Указатели символьные строки и функции. Строки и указатели в c++. C ++ Для начинающих. + ДЗ
+//Урок #63 - Видео №75-76.
+
+#include<iostream>
+using namespace std;
+
+void foo(const char *str)
+{
+	cout << strlen(str) << endl;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "RU");
+	//Пример строчного написания массива.
+	const char *strArr[] = {"Hello","World","PRINT"};
+	for (int i = 0; i < 3; i++)
+	{
+		cout << strArr[i] << endl;
+	}
+	cout << "=====================" << endl;
+	//Пример также строчного масива, написания его через указатель (*).
+	const char *str = "Hello""World""PRINT";
+	for (int i = 0; i < 15; i++)
+	{
+		cout << str[i] << endl;
+	}
+	cout << "=====================" << endl;
+	foo(str); //Данная функция, с помощью strlen подсчитывает количество хранящихся символов.
+}
+
