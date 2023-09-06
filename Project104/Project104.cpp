7cbc9088e0d5234b89d5fafcb4988518f9e69e61
SEGARSS <vim_branet@mail.ru>
2023-09-06 17:06:28 +0500

    конструкторы диструкторы в наследовательных классах в ооп

---
+++ Project104/Project104.cpp
@@ -0,0 +1,52 @@
+//Вызов конструктора базового класса из конструктора класса-наследника. Наследование. ООП C++ #102
+#include<iostream>
+#include<string>
+using namespace std;
+
+class A
+{
+public:
+	A()
+	{
+		msg = "Пустое сообщение";
+	}
+
+	A(string msg)
+	{
+		this->msg = msg;
+	}
+
+	A(string msg, int a)
+	{
+		this->msg = msg;
+	}
+
+	void PrintMsg()
+	{
+		cout << msg << endl;
+	}
+
+private:
+	string msg;
+};
+
+class B : public A
+{
+public:
+	B():A("Наше новое сообщение")
+	{
+		
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	B b;
+	b.PrintMsg();
+
+	return 0;
+}
+/*Вот такой запись B():A() можно вызвать конструктор по умолчанию. Или, если хотим другой, а у нас их там 4, один из
+которых по умолчанию, то мы в скобках A() пишем ту условие, которые были созданы в конструкторе.*/
