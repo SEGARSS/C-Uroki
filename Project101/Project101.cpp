b5e59da619f999565616291309ce0ed701e01e4f
SEGARSS <vim_branet@mail.ru>
2023-09-06 14:45:14 +0500

    поле protected: в ооп

---
+++ Project101/Project101.cpp
@@ -0,0 +1,36 @@
+//Модификаторы доступа при наследовании. private public protected Спецификаторы доступа. ООП. C++ #99 - Видео №115.
+#include<iostream>
+#include<string>
+using namespace std;
+
+class A
+{
+public:
+	string msgOne = "Сообщение один";
+private:
+	string msgTwo = "Сообщение два";
+protected:
+	string msgThree = "Сообщение три";
+};
+
+class B : public A
+{
+public:
+	void PrintMsg()
+	{
+		cout << msgOne << endl;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	B b;
+	b.PrintMsg();
+
+	return 0;
+}
+
+/*Зачастую использвать при наследии модификатор public - class B : public A. Поле protected:, не даёт напрямую пользваться её
+функцией, но, даёт пользваться её методом. Своеобразная методика защиты.*/
