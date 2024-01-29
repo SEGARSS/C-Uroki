423d2c8e1e9d4d2e8189699e5441b5a734edd311
SEGARSS <vim_branet@mail.ru>
2024-01-29 11:03:53 +0500

    итераторы

---
+++ Project141/Project141.cpp
@@ -0,0 +1,34 @@
+﻿//Итераторы stl c++ | prefix vs postfix | Библиотека стандартных шаблонов (stl) | Уроки | C++ | #4 - Видео №160. 
+
+#include<iostream>
+#include<list>
+#include<vector>
+#include<string>
+using namespace std;
+
+//-----------------------------------------------------------------------------------------
+template <typename T>
+void PrintList(const list<T> &lst)
+{
+	for (auto i = lst.cbegin(); i != lst.cend(); ++i)
+	{
+		cout << *i << endl;
+	}
+}
+//-----------------------------------------------------------------------------------------
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	list<int>::iterator it;
+
+	it++;
+	++it;
+
+	int a = 5;
+
+	cout << a++ << endl;
+
+	return 0;
+}
+/*Данный урок об оптимизации кода и его ускорения. Чаще всего использую ++a нежели чем а++, это ускорит работу кода.*/
