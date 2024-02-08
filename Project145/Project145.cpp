8215805c24abb92b1fc58430720bd56b286bcdc1
SEGARSS <vim_branet@mail.ru>
2024-02-08 15:41:32 +0500

    stl

---
+++ Project145/Project145.cpp
@@ -0,0 +1,16 @@
+﻿//array STL C++ | | Библиотека стандартных шаблонов (stl) | Уроки | C++ | #6 - Видео №164.
+
+#include<iostream>
+#include<array>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+	
+
+	return 0;
+}
+/*164 видео проработай*/
