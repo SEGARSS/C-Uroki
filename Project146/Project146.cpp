ace91894b9710e6fcbd1380a8c306a7029be61c1
SEGARSS <vim_branet@mail.ru>
2024-02-12 10:18:58 +0500

    работа deque

---
+++ Project146/Project146.cpp
@@ -0,0 +1,18 @@
+//deque stl c++ | Библиотека стандартных шаблонов (stl) | Уроки | C++ | #8 - Видео №165.
+
+#include<iostream>
+#include<vector>
+#include<deque>
+
+using namespace std;
+
+
+int main ()
+{
+	setlocale(LC_ALL,"ru");
+
+	deque<int> dq = {1, 9, 94};
+
+	return 0;
+}
+/*работа deque, аналогична работе с vector и list. Со своими приимуществами и недостатками*/
